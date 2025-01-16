44d0570eaa989dd834e34ff5b6c2290e28196e80
Nikita Nikitin <nnandpartners@gmail.com>
2025-01-16 20:05:35 +0300

    Initial filter config

---
+++ src/main/java/com/example/PPSecure/PpSecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.PPSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PPSecure/PpSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PPSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
